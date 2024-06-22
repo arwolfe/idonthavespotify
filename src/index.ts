b810139e3f58f3ab3f10f01d40f0f22e5c4c79eb
Anthony Wolfe <awolfe0513@gmail.com>
2024-06-22 18:30:58 -0500

    init commit

--- src/index.ts
+++ src/index.ts
@@ -7,20 +7,22 @@ import { logger } from './utils/logger';
 import { apiRouter } from './routes/api';
 import { pageRouter } from './routes/page';
 
+const asyncLogger = async ({ request }) => {
+  logger.info(
+    `${request.method} ${request.url} - ${request.headers.get('user-agent')}`
+  );
+}
+
+const staticAssets = staticPlugin({
+  prefix: '',
+  headers: {
+    'Cache-Control': 'public, max-age=86400',
+  },
+})
+
 export const app = new Elysia()
   .use(html())
-  .use(
-    staticPlugin({
-      prefix: '',
-      headers: {
-        'Cache-Control': 'public, max-age=86400',
-      },
-    })
-  )
-  .on('beforeHandle', async ({ request }) => {
-    logger.info(
-      `${request.method} ${request.url} - ${request.headers.get('user-agent')}`
-    );
-  })
+  .use(staticAssets)
+  .on('beforeHandle', asyncLogger)
   .use(apiRouter)
   .use(pageRouter);
